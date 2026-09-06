94e8d1c84cd40f5fd6ee215c2a08ea0f7e507c6a
mdriyazakondo <mdriyazakondo265@gmail.com>
2026-09-06 12:14:54 +0600

    logger file

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,23 @@ import express, { type Express, type Request, type Response } from "express";
 import userRouter from "./modules/user/user.route";
 import profileRoute from "./modules/profile/profile.route";
 import authRouter from "./modules/auth/login.route";
+import fs from "fs";
 const app: Express = express();
 
 // Middware
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log("first", Date.now(), req.method);
+  const log = `Method -> ${req.method} Time -> ${Date.now()} Url -> ${req.url}`;
+  fs.appendFile("logger-text", log, (error) => {
+    console.log(error);
+  });
+  next();
+});
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/profile", profileRoute);
 app.use("/api/v1/auth", authRouter);
